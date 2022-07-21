f75dcf881b7a55e3608da131150ab87fc750cec8
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-21 16:06:33 +0530

    added context

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = (props) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  return (
+    <StateContext.Provider
+      value={{
+        activeMenu,
+      }}
+    >
+      {props.children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
