f6c007389894fab99969036aa6c04423fb7861ca
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-21 09:58:35 +0530

    button styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Fisettings } from "react-icons";
+import { FiSettings } from "react-icons/fi";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import "./App.css";
@@ -12,8 +12,13 @@ const App = () => {
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
             <TooltipComponent content="Settings" position="Top">
-              <button>
-                <Fisettings />
+              <button
+                type="button"
+                className="text-3xl p-3
+              hover:drop-shadow-xl hover:bg-light-gray text-white"
+                style={{ backgroundColor: "gray", borderRadius: "50%" }}
+              >
+                <FiSettings />
               </button>
             </TooltipComponent>
           </div>
