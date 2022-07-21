aba27ca231b7ea9e4c0b5d4ec8e916371a36d809
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-21 09:50:40 +0530

    applied ToolTipComponent

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Fisettings } from "react-icons";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
 import "./App.css";
+
 const App = () => {
-  return <div className="underline text-3xl">App</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div className="flex relative dark:bg-main-dark-bg">
+          <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
+            <TooltipComponent content="Settings" position="Top">
+              <button>
+                <Fisettings />
+              </button>
+            </TooltipComponent>
+          </div>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
