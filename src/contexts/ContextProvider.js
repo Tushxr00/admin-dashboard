cadafa11d18f10fa4786399d068ec7d7795b301f
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-24 20:34:37 +0530

    created a function to close the side bar

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,6 +12,7 @@ const initialState = {
 export const ContextProvider = (props) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
+  const [screenSize, setScreenSize] = useState(undefined);
 
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
@@ -25,6 +26,8 @@ export const ContextProvider = (props) => {
         isClicked,
         setIsClicked,
         handleClick,
+        screenSize,
+        setScreenSize,
       }}
     >
       {props.children}
