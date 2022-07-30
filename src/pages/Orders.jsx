ab384723ae32a095301dbe2955d711e9ea7513cb
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-30 14:15:12 +0530

    formated code

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,13 +1,26 @@
 import React from "react";
-import { GridComponent, ColumnDirective, CoumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from "@syncfusion/ej2-react-grids";
+import {
+  GridComponent,
+  ColumnDirective,
+  CoumnDirective,
+  Resize,
+  Sort,
+  ContextMenu,
+  Filter,
+  Page,
+  ExcelExport,
+  PdfExport,
+  Edit,
+  Inject,
+} from "@syncfusion/ej2-react-grids";
 
-import { orderData, contextMenuItems, orderGrid } from '../data/dummy'
-import { Header } from "../components"
+import { orderData, contextMenuItems, orderGrid } from "../data/dummy";
+import { Header } from "../components";
 
 const Orders = () => {
   return (
     <div>
-          <Header category="Page" title="Orders" />
+      <Header category="Page" title="Orders" />
     </div>
   );
 };
