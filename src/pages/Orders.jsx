ec8aad5465241820abd74660c92bd9235a96483d
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-30 12:54:22 +0530

    started creating the order page

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { GridComponent,ColumnDirective,CoumnDirective,Resize,Sort, ContextMenu,Filter,Page,ExcelExport,PdfExport,Edit,Inject } from "@syncfusion/ej2-react-grids";
+
+import {orderData, contextMenuItems,orderGrid}from '../data/dummy'
+import {Header} from "../components"
 
 const Orders = () => {
-  return <div>Orders</div>;
+  return <div>
+    <Header category="Page" title="Orders" />
+  </div>;
 };
 
 export default Orders;
