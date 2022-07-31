4d4e8c449dc203e02b23052f1f0d3b8e7b89d43e
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-08-01 00:53:57 +0530

    adding inline pallete

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -7,6 +7,21 @@ const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Editor" />
+      <div className="text-center">
+        <div id="preview" />
+        <div className="flex flex-wrap justify-center items-center gap-20 ">
+          <div>
+            <p className="text-2xl font-semibold mt-2 mb-4">Inline Pallete</p>
+            <ColorPickerComponent
+              id="inline-pallete"
+              mode="Pallete"
+              modeSwitcher={false}
+              inline
+              showButtons={false}
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
