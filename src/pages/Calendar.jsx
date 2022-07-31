3df5d5ac87d3a3c548b85c28cd4cc425c17eb3d9
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-07-31 13:54:43 +0530

    created a calander

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -19,7 +19,14 @@ import { Header } from "../components";
 
 const Calendar = () => {
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">Calendar</div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Calendar" />
+      <ScheduleComponent>
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
   );
 };
 
