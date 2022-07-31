4a03d1b586562ba3ed626e52a90afac35ee2b6ff
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-08-01 00:40:29 +0530

    added data in the editor

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -14,9 +14,10 @@ import { Header } from "../components";
 
 const Editor = () => {
   return (
-    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="App" title="Editor" />
       <RichTextEditorComponent>
+        <EditorData />
         <Inject services={[HtmlEditor, Image, Link, QuickToolbar, Toolbar]} />
       </RichTextEditorComponent>
     </div>
