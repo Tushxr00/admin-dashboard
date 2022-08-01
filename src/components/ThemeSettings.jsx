aa722e1daaf2048f6a40386dde6d15c23133741f
Tushar <tushar.nandy@vidyamantra.com>
2022-08-01 09:59:50 +0530

    working on the themesettings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,4 +1,12 @@
 import React from "react";
+import {MdOutlineCancel} from "react-icons/md"
+import {BsCheck } from "react-icons/bs" 
+import { TooltipComponent } from "@syncfussion/ej2-react-popups"
+
+import {themeColors} from "../data/dummy"
+import { useStateContext} from "../contexts/ContextProvider"
+
+
 
 const ThemeSettings = () => {
   return <div>ThemeSettings</div>;
