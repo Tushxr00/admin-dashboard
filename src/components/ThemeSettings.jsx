bbae6b037500b6bfdeccd5bdf8999bd8c853e9d1
Tushar <tushar.nandy@vidyamantra.com>
2022-08-01 10:01:38 +0530

    fixed import

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import {MdOutlineCancel} from "react-icons/md"
-import {BsCheck } from "react-icons/bs" 
-import { TooltipComponent } from "@syncfussion/ej2-react-popups"
+import React from 'react';
+import { MdOutlineCancel } from 'react-icons/md';
+import { BsCheck } from 'react-icons/bs';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
 
-import {themeColors} from "../data/dummy"
-import { useStateContext} from "../contexts/ContextProvider"
-
+import { themeColors } from '../data/dummy';
+import { useStateContext } from '../contexts/ContextProvider';
 
 
 const ThemeSettings = () => {
   return <div
-  className = "bg-half-transparent w-screen fixed nav-item top-0 right-0"
-   >
+    className="bg-half-transparent w-screen fixed nav-item top-0 right-0"
+  >
 
   </div>;
 };
