530354ac6fdc40cf78c56daae1d7c4a75240b38f
Tushar <tushar.nandy@vidyamantra.com>
2022-08-02 09:49:07 +0530

    created context to handle the changes

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -13,7 +13,19 @@ export const ContextProvider = (props) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState("#02C9D7");
+  const [currentMode, setCurrentMode] = useState("Light");
+  const [themeSettings, setThemeSettings] = useState("Light");
 
+const setMode = (e)=>{
+  setCurrentMode(e.target.value)
+  localStorage.setItem('themeMode',e.target.value)
+}
+
+const setColor = (e)=>{
+  setCurrentColor(e.target.value)
+  localStorage.setItem('themeColor',e.target.value)
+}
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
   };
@@ -28,6 +40,12 @@ export const ContextProvider = (props) => {
         handleClick,
         screenSize,
         setScreenSize,
+        currentColor,
+        CurrentMode,
+        setCurrentColor,
+        setCurrentMode,
+        themeSettings, 
+        setThemeSettings
       }}
     >
       {props.children}
