c9052755fe388cd069616467a7f0c071559cc185
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2022-08-04 22:19:37 +0530

    formated the document

--- src/pages/Charts/ColorMapping.jsx
+++ src/pages/Charts/ColorMapping.jsx
@@ -1,40 +1,47 @@
 import React from "react";
 import {
   ChartComponent,
-  SeriesCollectionDirective, 
-  SeriesDirective, 
-  Inject, 
-  ColumnSeries, 
-  Category, 
-  Tooltip, 
-  Legend, 
-  RangeColorSettingsDirective, 
-  RangeColorSettingDirective
-} from "@syncfusion/ej2-react-charts"
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  ColumnSeries,
+  Category,
+  Tooltip,
+  Legend,
+  RangeColorSettingsDirective,
+  RangeColorSettingDirective,
+} from "@syncfusion/ej2-react-charts";
 
-import {  colorMappingData, ColorMappingPrimaryXAxis, ColorMappingPrimaryYAxis, rangeColorMapping} from "../../data/dummy"
-import { useStateContext } from "../../contexts/ContextProvider"
+import {
+  colorMappingData,
+  ColorMappingPrimaryXAxis,
+  ColorMappingPrimaryYAxis,
+  rangeColorMapping,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 import { Header } from "../../components";
 
 const ColorMapping = () => {
-
-  const {currentMode} = useStateContext()
+  const { currentMode } = useStateContext();
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-xl">
-    <Header category="Color Mappping" title="USA CLIMATE - WEATHER BY MONTH" />
-    <div className="w-full">
-    <ChartComponent
-    id="bar-chart"
-    height="420px"
-    primaryXAxis={ColorMappingPrimaryXAxis}
-    primaryYAxis={ColorMappingPrimaryYAxis}
-    chartArea={{border:{width:0}}}
-    tooltip={{enable:true}}
-    background={currentMode === "Dark"? "#33373E": "#fff" }
-    >
-      <Inject services={[ColumnSeries, Tooltip, Category, Legend]} />
-      <SeriesCollectionDirective>
+      <Header
+        category="Color Mappping"
+        title="USA CLIMATE - WEATHER BY MONTH"
+      />
+      <div className="w-full">
+        <ChartComponent
+          id="bar-chart"
+          height="420px"
+          primaryXAxis={ColorMappingPrimaryXAxis}
+          primaryYAxis={ColorMappingPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        >
+          <Inject services={[ColumnSeries, Tooltip, Category, Legend]} />
+          <SeriesCollectionDirective>
             <SeriesDirective
               dataSource={colorMappingData[0]}
               name="USA"
@@ -47,13 +54,14 @@ const ColorMapping = () => {
               }}
             />
           </SeriesCollectionDirective>
-      <RangeColorSettingsDirective>
-            {rangeColorMapping.map((item, index) => <RangeColorSettingDirective key={index} {...item} />)}
+          <RangeColorSettingsDirective>
+            {rangeColorMapping.map((item, index) => (
+              <RangeColorSettingDirective key={index} {...item} />
+            ))}
           </RangeColorSettingsDirective>
-    </ChartComponent>
+        </ChartComponent>
+      </div>
     </div>
-  </div>
-
   );
 };
 
--- src/pages/Charts/Financial.jsx
+++ src/pages/Charts/Financial.jsx
@@ -1,22 +1,26 @@
 import React from "react";
 import {
-  ChartComponent, 
-  SeriesCollectionDirective, 
-  SeriesDirective, 
-  Inject, 
-  HiloSeries, 
-  Tooltip, 
-  DateTime, 
-  Zoom, 
-  Logarithmic, 
-  Crosshair
-} from "@syncfusion/ej2-react-charts"
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  HiloSeries,
+  Tooltip,
+  DateTime,
+  Zoom,
+  Logarithmic,
+  Crosshair,
+} from "@syncfusion/ej2-react-charts";
 
-import { financialChartData, FinancialPrimaryXAxis, FinancialPrimaryYAxis} from "../../data/dummy"
-import { useStateContext } from "../../contexts/ContextProvider"
+import {
+  financialChartData,
+  FinancialPrimaryXAxis,
+  FinancialPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 import { Header } from "../../components";
 
-const date = new Date('2017, 1, 1');
+const date = new Date("2017, 1, 1");
 
 function filterValue(value) {
   if (value.x >= date) {
@@ -25,27 +29,34 @@ function filterValue(value) {
 }
 const returnValue = financialChartData.filter(filterValue);
 
-
 const ColorMapping = () => {
-
-  const {currentMode} = useStateContext()
+  const { currentMode } = useStateContext();
 
   return (
     <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-xl">
-    <Header category="Financial" title="AAPL Historical" />
-    <div className="w-full">
-    <ChartComponent
-    id="bar-chart"
-    height="420px"
-    primaryXAxis={FinancialPrimaryXAxis}
-    primaryYAxis={FinancialPrimaryYAxis}
-    chartArea={{border:{width:0}}}
-    tooltip={{ enable: true, shared: true }}
-    crosshair={{ enable: true, lineType: 'Vertical', line: { width: 0 } }}
-    background={currentMode === "Dark"? "#33373E": "#fff" }
-    >
-      <Inject services={[HiloSeries, Tooltip, DateTime, Logarithmic, Crosshair, Zoom]} />
-      <SeriesCollectionDirective>
+      <Header category="Financial" title="AAPL Historical" />
+      <div className="w-full">
+        <ChartComponent
+          id="bar-chart"
+          height="420px"
+          primaryXAxis={FinancialPrimaryXAxis}
+          primaryYAxis={FinancialPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          tooltip={{ enable: true, shared: true }}
+          crosshair={{ enable: true, lineType: "Vertical", line: { width: 0 } }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+        >
+          <Inject
+            services={[
+              HiloSeries,
+              Tooltip,
+              DateTime,
+              Logarithmic,
+              Crosshair,
+              Zoom,
+            ]}
+          />
+          <SeriesCollectionDirective>
             <SeriesDirective
               dataSource={returnValue}
               xName="x"
@@ -56,9 +67,9 @@ const ColorMapping = () => {
               high="high"
             />
           </SeriesCollectionDirective>
-    </ChartComponent>
+        </ChartComponent>
+      </div>
     </div>
-  </div>
   );
 };
 
