23272b926088cdeaf2511e47787cb3a9573bcb6e
Tushar Nandy <97872103+Tushxr00@users.noreply.github.com>
2023-09-22 13:58:54 +0530

    fixed footer background color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => (
-  <div className="pt-24  dark:bg-main-dark-bg">
+  <div className="pt-24  dark:bg-main-dark-bgq">
     <p className="dark:text-gray-200 text-gray-700 text-center p-20">
       © 2022 All rights reserved by Shoppy.com
     </p>
